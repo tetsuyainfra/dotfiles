991847ac344828721ffc698dd9084db5e17de7c1
tetsuyainfra <tetsuya.infra@gmail.com>
2019-01-07 00:00:11 +0900

    fix

--- fish/conf.d/rbenv.fish
+++ fish/conf.d/rbenv.fish
@@ -1,11 +1,19 @@
 
+# update
+if test -e $HOME/.rbenv
+	status --is-interactive; and pushd $HOME/.rbenv; and git pull --verbose; and popd
+end
+
+if test -e $HOME/.rbenv/plugins/ruby-build
+	status --is-interactive; and pushd $HOME/.rbenv/plugins/ruby-build; and git pull --verbose; and popd
+end
+
+# setup
 if test -e $HOME/.rbenv/bin 
 	set -x PATH $HOME/.rbenv/bin   $PATH
 	set -x PATH $HOME/.rbenv/shims $PATH
 end
 
 if which rbenv > /dev/null 2>&1
-	status --is-interactive; and pushd $HOME/.rbenv; and git pull --verbose; and popd
-	status --is-interactive; and pushd $HOME/.rbenv/plugins/ruby-build; and git pull --verbose; and popd
 	status --is-interactive; and source (rbenv init -|psub)
 end
