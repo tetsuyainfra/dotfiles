437938fae89f2e27039c50ea7c6f5276a8d99ca0
tetsuyainfra <tetsuya.infra@gmail.com>
2019-01-13 14:27:33 +0900

    fix

--- fish/conf.d/rbenv.fish
+++ fish/conf.d/rbenv.fish
@@ -1,12 +1,12 @@
 
 # update
-if test -e $HOME/.rbenv
-	status --is-interactive; and pushd $HOME/.rbenv; and git pull --verbose; and popd
-end
+#if test -e $HOME/.rbenv
+#	status --is-interactive; and pushd $HOME/.rbenv; and git pull --verbose; and popd
+#end
 
-if test -e $HOME/.rbenv/plugins/ruby-build
-	status --is-interactive; and pushd $HOME/.rbenv/plugins/ruby-build; and git pull --verbose; and popd
-end
+#if test -e $HOME/.rbenv/plugins/ruby-build
+#	status --is-interactive; and pushd $HOME/.rbenv/plugins/ruby-build; and git pull --verbose; and popd
+#end
 
 # setup
 if test -e $HOME/.rbenv/bin 
