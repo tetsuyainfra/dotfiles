c59e5660b13849841401111a779ec0216293563b
tetsuyainfra <tetsuya.infra@gmail.com>
2019-07-06 22:21:50 +0900

    add use mirror on apt

--- dev-env/dev-setup.linux.ubuntu.sh
+++ dev-env/dev-setup.linux.ubuntu.sh
@@ -8,6 +8,17 @@ DOT_DIR=$(cd $(dirname $0);pwd)
 
 echo "DOT_DIR: $DOT_DIR"
 
+## apt use mirror
+not_mirror_count=`grep --count '^deb.*http://archive.ubuntu.com/' /etc/apt/sources.list`
+if [ $not_mirror_count -gt 0 ]; then
+# select which one
+# sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu/%http://jp.archive.ubuntu.com/ubuntu/%g" /etc/apt/sources.list
+# sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu/%http://ftp.iij.ad.jp/pub/linux/ubuntu/archive/%g" /etc/apt/sources.list
+  sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu/%http://ftp.jaist.ac.jp/pub/Linux/ubuntu/%g" /etc/apt/sources.list
+  apt update
+fi
+
+
 ##
 if [ type fish > /dev/null 2>&1 ]; then
   apt-add-repository -y ppa:fish-shell/release-3
