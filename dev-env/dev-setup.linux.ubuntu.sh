2f6f6f5e6bfcd698e5e0d641e88a45561ae20739
tetsuyainfra <tetsuya.infra@gmail.com>
2019-07-17 20:07:25 +0900

    update

--- dev-env/dev-setup.linux.ubuntu.sh
+++ dev-env/dev-setup.linux.ubuntu.sh
@@ -26,6 +26,28 @@ if [ type fish > /dev/null 2>&1 ]; then
   apt-get install -y fish
 fi
 
+## Linux-brew
+apt install -y \
+  build-essential \
+  curl git m4 ruby texinfo \
+  libbz2-dev \
+  libcurl4-openssl-dev \
+  libexpat-dev \
+  libncurses-dev \
+  zlib1g-dev \
+  gettext
+# gettextはおまじない
+if [ ! -e /home/linuxbrew ]; then
+  sudo -u ${SUDO_USER:-${USER}} sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+  # config for *sh
+  test -d /home/linuxbrew/.linuxbrew && eval $(sudo -u ${SUDO_USER:-${USER}} /home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+fi
+
+exit
+
 ## ruby
 apt install -y \
 	autoconf \
