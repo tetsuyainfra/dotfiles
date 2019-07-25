d677f783e33213884578920929c780abed55558c
tetsuyainfra <tetsuya.infra@gmail.com>
2019-07-25 18:07:32 +0900

    add nodejs install-script

---
+++ dev-env/nodejs.ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -x
+
+DOT_DIR=$(cd $(dirname $0);pwd)
+echo "DOT_DIR: $DOT_DIR"
+
+
+
+## Nodejs
+if !(which node >/dev/null 2>&1) ; then
+  # next LTS
+  # curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+  curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+fi
