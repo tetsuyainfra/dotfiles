1c60f496beeecd06900d8a1b05f4bdbd586a9f9e
tetsuyainfra <tetsuya.infra@gmail.com>
2019-08-03 20:11:25 +0900

    add gnuplot, fc-config

--- dev-env/x.ubuntu.sh
+++ dev-env/x.ubuntu.sh
@@ -12,4 +12,10 @@ set -u
 # Show debug info
 set -x
 
-apt install x11-apps x11-utils x11-xserver-utils fonts-ipafont
+apt install x11-apps x11-utils x11-xserver-utils fonts-ipafont \
+              gnuplot gnuplot-x11
+
+if [ ! -e /usr/share/fonts/windows ] ; then
+  ln -s /mnt/c/Windows/Fonts /usr/share/fonts/windows
+  fc-cache -fv
+fi
