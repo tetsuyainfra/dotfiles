463e91b045efe26c85e273598ea061f99aec90a1
tetsuyainfra <tetsuyainfra@gmail.com>
2020-09-07 16:56:09 +0900

    update

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ansible-playbook setup.yml
+set -ex
+
+if [ ! -e "vars/dot_vars.yml" ] ; then
+  ansible-playbook -i inventories/local playbooks/init_vars.yml
+fi
+
+ansible-playbook -i inventories/local setup.yml
