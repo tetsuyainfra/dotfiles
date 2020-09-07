0b45905a9611b128b3340bdb7b8625ccc20cc4ad
tetsuyainfra <tetsuyainfra@gmail.com>
2020-09-07 16:59:43 +0900

    update

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+DOT_ANSIBLE_DIR=$(cd $(dirname $0); pwd)
 
 set -ex
+echo $DOT_ANSIBLE_DIR
 
-if [ ! -e "vars/dot_vars.yml" ] ; then
-  ansible-playbook -i inventories/local playbooks/init_vars.yml
-fi
+pushd $DOT_ANSIBLE_DIR
+  if [ ! -e "vars/dot_vars.yml" ] ; then
+    ansible-playbook -i inventories/local playbooks/init_vars.yml
+  fi
 
-ansible-playbook -i inventories/local setup.yml
+  ansible-playbook -i inventories/local setup.yml
+popd
