60f629376caf0a6e323ac9ccd4a48fb3080ebe22
tetsuyainfra <tetsuyainfra@gmail.com>
2020-09-07 15:09:25 +0900

    support ansible

---
+++ setup_ansible.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -ex
+
+USE_PYTHON_VERSION=3.8.5
+
+# pyenv setup
+if [ ! -e ~/.pyenv ]; then
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+fi
+if [ ! -e ~/.pyenv/plugins/pyenv-virtualenv ]; then
+	git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+fi
+if [ -z "$PYENV_SHELL" ] ; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+  fi
+fi
+
+# install python and ansible in directory
+pyenv virtualenv -f $USE_PYTHON_VERSION use_ansible
+pushd ansible
+  pyenv local use_ansible
+  pip3 install ansible
+popd
+
