a950e57bb999ada3ff2aff1fe895f2d246b59694
tetsuyainfra <tetsuyainfra@gmail.com>
2022-02-12 09:44:26 +0000

    update packer

--- dev-env/ubuntu_maas_packer.sh
+++ dev-env/ubuntu_maas_packer.sh
@@ -12,5 +12,5 @@ DOT_DIR=`dotdir`
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update
-sudo apt-get install -y packer qemu-system-x86 xorriso
+sudo apt-get install -y packer qemu-system-x86 xorriso python3-pip
 
