8f8835f7e333ab98d46ceefaec53d3190c03e551
tetsuyainfra <tetsuyainfra@gmail.com>
2022-06-13 22:38:27 +0900

    Ubuntuで利用するprepareスクリプトを更新

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-
 # dotfile.git
 
 [tetsuyainfra/dotfile.git](https://github.com/tetsuyainfra/dotfile.git)
 
 ## how to use: Ubuntu@WSL2
+
 ```
 # set repository url from https to git.
 ./setup/modify_git_remote_repo_url.sh
@@ -18,6 +18,7 @@ ENABLE_ALL=1 ./install_ubuntu.sh
 ```
 
 ## how to use: CommandLine(Windows)
+
 ```
 # set repository url from https to git.
 ./setup/modify_git_remote_repo_url.bat
@@ -29,26 +30,26 @@ ENABLE_ALL=1 ./install_ubuntu.sh
 
 ```
 
-
 # 仕組みのメモ
-- dotterでファイルの管理をする
-  - bin/dotter, bin/dotter.exeを含める
-- OSの違いはprepare_{windows.bat|unix.sh}で吸収する
-  - バッチファイル内で.dotter/local.tomlをOS毎のスケルトンをコピーする
-- prepare_* は 設定ファイルのコピー/リンク
-- setup_* は基本ソフトウェアのインストール
 
+- dotter でファイルの管理をする
+  - dotter, dotter.exe バイナリを含める(dotfiles ディレクトリ内で直接実行するので bin/には含めない)
+- OS の違いは prepare\_{windows.bat|unix.sh}で吸収する
+  - バッチファイル内で.dotter/local.toml を OS 毎のスケルトンをコピーする
+- prepare\_\* は 設定ファイルのコピー/リンク
+- setup\_\* は基本ソフトウェアのインストール
 
 # Bash
-- Ubuntu環境では .bash_aliases にリンクを張っておけば
-  bashrcの書き換えなどをしなくて済む
 
+- Ubuntu 環境では .bash_aliases にリンクを張っておけば
+  bashrc の書き換えなどをしなくて済む
 
 # 便利そう
+
 - https://speakerdeck.com/hsbt/kai-fa-huan-jing-hefalsekodawari?slide=8
-find -> fdfind
-grep -> ripgrep(rg)
-sed -> sd
-cat -> bat
-dig -> dog carate.ioに登録されてない
-man -> tealdeer(tldr)
+  find -> fdfind
+  grep -> ripgrep(rg)
+  sed -> sd
+  cat -> bat
+  dig -> dog carate.io に登録されてない
+  man -> tealdeer(tldr)
---
+++ prepare_linux.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#set -e
+# detect what we have
+if [  $(uname -a | grep -c "Microsoft") -eq 1 ]; then
+    export ISWSL=1 # WSL 1
+elif [ $(uname -a | grep -c "microsoft") -eq 1 ]; then
+    export ISWSL=2 # WSL 2
+else
+    export ISWSL=0
+fi
+
+function dotdir() {
+  echo "$(cd $(dirname $0); pwd)"
+}
+DOT_DIR=`dotdir`
+
+
+function ConfirmGitInfo(){
+  GIT_USERNAME=${GIT_USERNAME:=$(git config --get user.name)}
+  if [ -z "${GIT_USERNAME}" ] ; then
+    read -p "GIT_USERNAME:" GIT_USERNAME
+  fi
+  GIT_EMAIL=${GIT_EMAIL:=$(git config --get user.email)}
+  if [ -z "${GIT_EMAIL}" ] ; then
+    read -p "GIT_EMAIL:" GIT_EMAIL
+  fi
+}
+
+function ConfirmExecution() {
+echo "GIT_USERNAME: $GIT_USERNAME"
+echo "GIT_EMAIL   : $GIT_EMAIL"
+
+read -p "ok? (y/N): " yn
+case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
+}
+
+
+ConfirmGitInfo
+set -e
+ConfirmExecution
+
+set -x
+
+
+DOTTER_LOCAL_FILE="${DOT_DIR}/.dotter/local.toml"
+DOTTER_LOCAL_SKEL="${DOT_DIR}/.dotter/skel/local_ubuntu.toml"
+
+if [ ! -e "${DOTTER_LOCAL_FILE}" ] ; then
+cp ${DOTTER_LOCAL_SKEL} ${DOTTER_LOCAL_FILE}
+fi
+
+## dotter
+pushd ${DOT_DIR}
+
+dotter
+
+## GIT
+git config -f ~/.config/git/config.local user.name ${GIT_USERNAME}
+git config -f ~/.config/git/config.local user.email ${GIT_EMAIL}
+
+popd
+
+## WSL2 support ssh
+#if [ ${ISWSL} -eq 2 ]; then
+#  windows_destination="/mnt/d/opt/bin/wsl2-ssh-pageant.exe"
+#  linux_destination="$HOME/.ssh/wsl2-ssh-pageant.exe"
+#  mkdir -p $HOME/.ssh
+#  chmod +x "$windows_destination"
+#  ln -s $windows_destination $linux_destination
+#fi
+
+
